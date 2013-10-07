1f266ff852f6f69928ffdc5342c1df7cd4a5bcd9
Ken Huh <ken123777@gmail.com>
2013-10-07 15:39:39 -0700

    Messages refresh every 2 seconds

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,30 @@
-// YOUR CODE HERE:
+(function(){
+
+	var displayChats = function(dataObject){
+		var $chats = $('#chats');
+
+		for( var i = 0; i < dataObject.results.length; i++ ){
+			var text = "<div class='msg'>'" + dataObject.results[i].username 
+			+ "' says, '" + dataObject.results[i].text + "'</div>";
+			$chats.append(text);
+		}
+	};
+
+	$.ajax({
+		url: 'https://api.parse.com/1/classes/chatterbox',
+		type: 'GET',
+		dataType: 'JSON',
+		data: 'data',
+		success: function(data){
+			// console.log(data);  // data.results[i].text username roomname updatedAt createdAt
+			setInterval(function(){ 
+				$('#chats').empty();
+				displayChats(data);
+			}, 2000);
+		},
+		error: function(data){
+			console.log("ERROR: ", data);
+		}
+	});	
+
+})();
